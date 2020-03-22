e41f8c967b0e626ac8fc948e8371e04b34448c92
Bruno Coelho <brunoleocoelho@gmail.com>
2020-03-21 22:05:27 -0300

    Acerto nos componentes que chamam o Header

--- src/screens/CriarLista.js
+++ src/screens/CriarLista.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View, ScrollView, FlatList, } from 'react-native'
 
 import Header from '../components/Header/Header';
 
-import { colorTheme, stdFontSizes } from '../util/constants';
+import { stdFontSizes } from '../util/constants';
 
 import feedList from '../data/product-list.json'; //lista Mock de produtos
 import ProdutosLista from '../components/ProdutosLista';
@@ -30,7 +30,7 @@ const CriarLista = () => {
 
         const newTotal = newSelected.reduce((prvVal, curVal) => (prvVal + curVal.price), 0);
 
-        console.log("SELECTED", newSelected);
+        // console.log("SELECTED", newSelected);
         Promise.all([
             setSelected(newSelected),
             setTotalRS(newTotal)
@@ -41,10 +41,8 @@ const CriarLista = () => {
         <View  style={styles.mainContainer}>
             <Header 
                 title={'Criar Lista'}
-                titleColor={'#FFF'}
-                titleAlign={'left'} 
-                titlePosition={'left'}
-                backgroundColor={colorTheme} 
+                leftProps = {{ icon: 'menu', action: () => alert('RIGHT TAPPED!!!') }}
+                rightProps = {{ icon: 'more-vert', action: () => alert('RIGHT TAPPED!!!') }}
             />
 
             <Text style={styles.totalizador}>{`Selecionados: ${selected.length}  Total R$ ${totalRS.toFixed(2) || 0}`}</Text>
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,8 +3,6 @@ import { StyleSheet, Text, View, ScrollView, FlatList, } from 'react-native'
 
 import Header from '../components/Header/Header';
 
-import { colorTheme } from '../util/constants';
-
 /** Home screen for the app */
 const Home = () => {
 
@@ -12,10 +10,8 @@ const Home = () => {
         <View  style={styles.mainContainer}>
             <Header 
                 title={'Home'}
-                titleColor={'#FFF'}
-                titleAlign={'left'} 
-                backgroundColor={colorTheme} 
             />
+
             <View style={styles.bodyContainer}>
                 <Text>HOME SCREEN</Text>
             </View>
